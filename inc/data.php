5a1da669255a16fbb76f583482421b556e600925
stephannycharry <39140447+stephannycharry@users.noreply.github.com>
2018-05-30 19:20:55 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,182 @@
+<?php
+$catalog = [];
+
+//Books*
+$catalog[101] = [
+    "title" => "The Story of a Shipwrecked Sailor",
+    "img" => "img/media/GabrielGarcia.png",
+   "genre" => "Self-help",
+   "format" => "Paperback",
+   "year" => 1986,
+   "category" => "Books",
+   "authors" => [
+       "Gabriel Garcia Marquez",
+      
+   ],
+   "publisher" => "Knopf",
+   "isbn" => '0394548108'
+];
+
+$catalog[102] = [
+    "title" => "I am Malala",
+    "img" => "img/media/bookofjoy.jpg",
+   "genre" => "Biography, Autobiography",
+   "format" => "Paperback",
+   "year" => October 8,2013,
+   "category" => "Books",
+   "authors" => [
+       "Malala Yousafzai",
+       "Christina Lamb",
+      
+   ],
+   "publisher" => " Orion Publishing Co",
+   "isbn" => '1780226586'
+];
+
+$catalog[103] = [
+    "title" => "Ghost Boy",
+    "img" => "img/media/bookofjoy.jpg",
+   "genre" => "Biography, Autobiography",
+   "format" => "Paperback",
+   "year" => October 8,2013,
+   "category" => "Books",
+   "authors" => [
+       "Martin Pistorius"
+      
+   ],
+   "publisher" => "Simon & Schuster Ltd",
+   "isbn" => '147115100X'
+];
+
+$catalog[104] = [
+    "title" => "El Perfume",
+    "img" => "img/media/bookofjoy.jpg",
+   "genre" => "Horror Fiction,Mystery",
+   "format" => "Paperback",
+   "year" => 30 Dec 2006,
+   "category" => "Books",
+   "authors" => [
+       " Patrick Süskind"
+      
+   ],
+   "publisher" => "Editorial Seix Barral, S.A.",
+   "isbn" => '843221745X'
+];
+
+
+*//Movies*
+$catalog[201] = [
+   "title" => "Arrival",
+   "img" => "img/media/avatar.jpg",
+   "genre" => "Drama",
+   "format" => "DVD",
+   "year" => 2016,
+   "category" => "Movies",
+   "director" => "Denis Villeneuve",
+   "writers" => [
+       " Eric Heisserer"
+
+   ],
+   "stars" => [
+       "Amy Adams",
+       "Jeremy Renner",
+       "Forest Whitaker"
+   ]
+];
+
+$catalog[202] = [
+   "title" => "The Butterfly Effect",
+   "img" => "img/media/avatar.jpg",
+   "genre" => "Sci-Fi, Thriller",
+   "format" => "DVD",
+   "year" => 2004,
+   "category" => "Movies",
+   "director" => "Eric Bress, J. Mackye Gruber",
+   "writers" => [
+       " Eric Bress, J. Mackye Gruber"
+
+   ],
+   "stars" => [
+       "Amy Smart",
+       "Ashton Kutcher",
+       "Melora Walters"
+   ]
+];
+
+$catalog[203] = [
+   "title" => "Hacksaw Ridge",
+   "img" => "img/media/avatar.jpg",
+   "genre" => "Biography,Drama,History",
+   "format" => "DVD",
+   "year" => 2016,
+   "category" => "Movies",
+   "director" => "Mel Gibson",
+   "writers" => [
+       "Robert Schenkkan, Andrew Knight"
+
+   ],
+   "stars" => [
+       "Andrew Garfield",
+       "Sam Worthington",
+       "Luke Bracey"
+   ]
+];
+
+$catalog[204] = [
+   "title" => "Interstellar",
+   "img" => "img/media/avatar.jpg",
+   "genre" => "Adventure,Drama.Sci-fi",
+   "format" => "DVD",
+   "year" => 2014,
+   "category" => "Movies",
+   "director" => "Christopher Nolan",
+   "writers" => [
+       "Jonathan Nolan, Christopher Nolan"
+
+   ],
+   "stars" => [
+       "Matthew McConaughey",
+       "Anne Hathaway",
+       "Jessica Chastain"
+   ]
+];
+*//Music*
+$catalog[301] = [
+   "title" => "La Dulzura",
+   "img" => "img/media/beethoven.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Cultura Profetica"
+];
+
+$catalog[302] = [
+   "title" => "Amanecer",
+   "img" => "img/media/beethoven.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Bomba Stereo"
+];
+
+$catalog[303] = [
+   "title" => "Viva la Vida",
+   "img" => "img/media/beethoven.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Cold Play"
+];
+
+$catalog[304] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/beethoven.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Vicente Garcia"
+];
